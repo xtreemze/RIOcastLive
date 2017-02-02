b0952eb4ebaa0d5548a1674a0dfba36399b34812
Carlos Velasco <carlos@2activedesign.com>
2017-02-02 18:59:30 +0100

    Cleanup

--- js/airtime/login/login.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-    $("#username").focus();
-});
-
-$(document).ready(function() {
-    $("#submit").click(function() {
-        $.cookie("airtime_locale", $("#locale").val(), {path: '/'});
-    });
-});
--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext || window
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
 window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
 try {
-	this.audioContext = new AudioContext();
+  this.audioContext = new AudioContext();
 } catch (e) {
-	this._updateInfo("!Your browser does not support AudioContext", false);
-// console.log(e);
+  this._updateInfo("!Your browser does not support AudioContext", false);
+  // console.log(e);
 }
 /*
  █████  ██    ██ ██████  ██  ██████       ██████  ██████       ██ ███████  ██████ ████████
@@ -20,13 +20,15 @@ try {
 // Create Audio Object and Decalre Properties
 var audioElement = new Audio();
 var smartSource = function () {
-	if (audioElement.canPlayType("audio/mpeg")) {
-		audioElement.setAttribute("src", "http://192.30.164.78:8000/rio");
-		$("#mp3").addClass("active");
-	} else if (audioElement.canPlayType("audio/ogg")) {
-		audioElement.setAttribute("src", "http://192.30.164.78:8000/airtime");
-		$("#ogg").addClass("active");
-	}
+  if (audioElement.canPlayType("audio/mpeg")) {
+    audioElement.setAttribute("src", "http://192.30.164.78:8000/bahia");
+    $("#mp3")
+      .addClass("active");
+  } else if (audioElement.canPlayType("audio/ogg")) {
+    audioElement.setAttribute("src", "http://192.30.164.78:8000/airtime");
+    $("#ogg")
+      .addClass("active");
+  }
 };
 smartSource();
 audioElement.controls = false;
@@ -41,62 +43,78 @@ audioElement.id = "a";
  ██████  ██████  ██   ████    ██    ██   ██  ██████  ███████ ███████
 */
 $("#audio-visual")[0].appendChild(audioElement);
-$("#pause").click(function () {
-	audioElement.pause();
-	$("#pause").addClass("btn-warning");
-	$("#play").removeClass("btn-success");
-	$("#bar").removeClass("active");
-});
-$("#reconnect").click(function () {
-	$("#bar").removeClass("active").removeClass("progress-bar-success").addClass("progress-bar-warning");
-	$("#pause").removeClass("btn-warning");
-	smartSource();
-	checkSource();
-	$("#play").removeClass("btn-success");
-	play();
-});
+$("#pause")
+  .click(function () {
+    audioElement.pause();
+    $("#pause")
+      .addClass("btn-warning");
+    $("#play")
+      .removeClass("btn-success");
+    $("#bar")
+      .removeClass("active");
+  });
+$("#reconnect")
+  .click(function () {
+    $("#bar")
+      .removeClass("active")
+      .removeClass("progress-bar-success")
+      .addClass("progress-bar-warning");
+    $("#pause")
+      .removeClass("btn-warning");
+    smartSource();
+    checkSource();
+    $("#play")
+      .removeClass("btn-success");
+    play();
+  });
 var play = function () {
-	audioElement.play();
-	$("#pause").removeClass("btn-warning");
-	$("#bar").addClass("active");
-	$("#play").addClass("btn-success");
+  audioElement.play();
+  $("#pause")
+    .removeClass("btn-warning");
+  $("#bar")
+    .addClass("active");
+  $("#play")
+    .addClass("btn-success");
 };
-$("#play").click(function () {
-	play();
-});
+$("#play")
+  .click(function () {
+    play();
+  });
 play();
-audioElement.addEventListener("stalled", function () {
-	// console.log("Audio Element Stall");
-	redZone();
-});
 audioElement.addEventListener("loadstart", function () {
-	// console.log("Audio Element Loading...");
-	$("#bar").addClass("progress-bar-info").removeClass("progress-bar-danger").removeClass("progress-bar-warning").addClass("active");
-	$("#play").removeClass("btn-danger").removeClass("btn-success");
-	$("#pause").removeClass("btn-danger");
+  // console.log("Audio Element Loading...");
+  $("#bar")
+    .addClass("progress-bar-info")
+    .removeClass("progress-bar-danger")
+    .removeClass("progress-bar-warning")
+    .addClass("active");
+  $("#play")
+    .removeClass("btn-danger")
+    .removeClass("btn-success");
+  $("#pause")
+    .removeClass("btn-danger");
 });
 audioElement.addEventListener("canplay", function () {
-	// console.log("Audio Element Ready to Play");
-	$("#bar").addClass("progress-bar-success").removeClass("progress-bar-info").removeClass("progress-bar-danger").removeClass("progress-bar-warning");
-	$("#play").removeClass("btn-danger").removeClass("btn-success");
-	$("#pause").removeClass("btn-danger");
-	if (audioElement.paused) {
-		// console.log("Can play but paused");
-	} else {
-		$("#play").addClass("btn-success");
-		$("#pause").removeClass("btn-warning");
-	}
+  // console.log("Audio Element Ready to Play");
+  $("#bar")
+    .addClass("progress-bar-success")
+    .removeClass("progress-bar-info")
+    .removeClass("progress-bar-danger")
+    .removeClass("progress-bar-warning");
+  $("#play")
+    .removeClass("btn-danger")
+    .removeClass("btn-success");
+  $("#pause")
+    .removeClass("btn-danger");
+  if (audioElement.paused) {
+    // console.log("Can play but paused");
+  } else {
+    $("#play")
+      .addClass("btn-success");
+    $("#pause")
+      .removeClass("btn-warning");
+  }
 });
-audioElement.addEventListener("abort", function () {
-	// console.log("Audio Element Abort");
-	redZone();
-});
-var redZone = function () {
-	// console.log("Red Zoned");
-	// $('#bar').addClass("progress-bar-danger").removeClass("progress-bar-success").removeClass("active")
-	// $('#play').addClass("btn-danger")
-	// $('#pause').addClass("btn-danger").removeClass("btn-warning")
-};
 /*
  ██████  █████  ███    ██ ██    ██  █████  ███████
 ██      ██   ██ ████   ██ ██    ██ ██   ██ ██
@@ -107,12 +125,12 @@ var redZone = function () {
 var canvas = $("#c")[0];
 var canvasCtx = canvas.getContext("2d");
 // Create Analyzer
-var context = new (window.AudioContext || window.webkitAudioContext)();
+var context = new(window.AudioContext || window.webkitAudioContext)();
 var analyser = context.createAnalyser();
 analyser.fftSize = 256;
-analyser.smoothingTimeConstant = 0.5;
-// analyser.minDecibels = -90
-// analyser.maxDecibels = -30
+analyser.smoothingTimeConstant = 0.2;
+analyser.minDecibels = -98
+analyser.maxDecibels = -32
 // draw the analyser to the canvas
 /*
 ██████  ██████   █████  ██     ██
@@ -121,37 +139,38 @@ analyser.smoothingTimeConstant = 0.5;
 ██   ██ ██   ██ ██   ██ ██ ███ ██
 ██████  ██   ██ ██   ██  ███ ███
 */
-function freqAnalyser () {
-	window.requestAnimationFrame(freqAnalyser);
-	var sum;
-	var average;
-	var bar_width;
-	var scaled_average;
-	var num_bars = 24;
-	var data = new Uint8Array(128);
-	analyser.getByteFrequencyData(data);
-	if (!analyser) {
-		$("#vis").html(data[0]);
-	}
-	// clear canvas
-	canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-	var gradient = canvasCtx.createLinearGradient(0, canvas.height, 0, 0);
-	gradient.addColorStop(0, "#28B62C");
-	gradient.addColorStop(0.8, "#F5D802");
-	gradient.addColorStop(1, "red");
-	canvasCtx.fillStyle = gradient;
-	// DRAW Individual Bars
-	var bin_size = Math.floor((data.length - 40) / num_bars);
-	for (var i = 0; i < num_bars; i++) {
-		sum = 0;
-		for (var j = 0; j < bin_size; j++) {
-			sum += data[(i * bin_size) + j];
-		}
-		average = sum / bin_size;
-		bar_width = canvas.width / num_bars;
-		scaled_average = (average / 256) * canvas.height;
-		canvasCtx.fillRect(i * bar_width, canvas.height, bar_width / 1.1, -scaled_average);
-	}
+function freqAnalyser() {
+  window.requestAnimationFrame(freqAnalyser);
+  var sum;
+  var average;
+  var bar_width;
+  var scaled_average;
+  var num_bars = 16;
+  var data = new Uint8Array(128);
+  analyser.getByteFrequencyData(data);
+  if (!analyser) {
+    $("#vis")
+      .html(data[0]);
+  }
+  // clear canvas
+  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+  var gradient = canvasCtx.createLinearGradient(0, canvas.height, 0, 0);
+  gradient.addColorStop(0, "#4caf50");
+  gradient.addColorStop(0.5, "#cddc39");
+  gradient.addColorStop(0.98, "#f44336");
+  canvasCtx.fillStyle = gradient;
+  // DRAW Individual Bars
+  var bin_size = Math.floor((data.length - 40) / num_bars);
+  for (var i = 0; i < num_bars; i++) {
+    sum = 0;
+    for (var j = 0; j < bin_size; j++) {
+      sum += data[(i * bin_size) + j];
+    }
+    average = sum / bin_size;
+    bar_width = canvas.width / num_bars;
+    scaled_average = (average / 256) * canvas.height;
+    canvasCtx.fillRect(i * bar_width, canvas.height, bar_width / 1.1, -scaled_average);
+  }
 }
 /*
 ███    ██  █████  ██    ██ ██  ██████   █████  ████████ ██  ██████  ███    ██
@@ -169,19 +188,25 @@ function freqAnalyser () {
 //   checkSource()
 // }
 var checkSource = function () {
-	if ($("#a")[0].src === "http://192.30.164.78:8000/rio") {
-		$("#mp3").addClass("active");
-		$("#ogg").removeClass("active");
-	} else if ($("#a")[0].src === "http://192.30.164.78:8000/airtime") {
-		$("#mp3").removeClass("active");
-		$("#ogg").addClass("active");
-	}
+  if ($("#a")[0].src === "http://192.30.164.78:8000/rio") {
+    $("#mp3")
+      .addClass("active");
+    $("#ogg")
+      .removeClass("active");
+  } else if ($("#a")[0].src === "http://192.30.164.78:8000/airtime") {
+    $("#mp3")
+      .removeClass("active");
+    $("#ogg")
+      .addClass("active");
+  }
 };
 
-function respondCanvas () {
-	canvas.width = $("#bottom").width(); // max width
-	canvas.height = $("#controlAudio").height() - 72; // max height
-	// Call a function to redraw other content (texts, images etc)
+function respondCanvas() {
+  canvas.width = $("#bottom")
+    .width(); // max width
+  canvas.height = $("#controlAudio")
+    .height() - 72; // max height
+  // Call a function to redraw other content (texts, images etc)
 }
 /*
 ██████   ██████   ██████     ██████  ███████  █████  ██████  ██    ██
@@ -190,34 +215,39 @@ function respondCanvas () {
 ██   ██ ██    ██ ██          ██   ██ ██      ██   ██ ██   ██    ██
 ██████   ██████   ██████     ██   ██ ███████ ██   ██ ██████     ██
 */
-$("document").ready(function () {
-	// connect AudioElement to Analyzer via source1 variable then Analyzer to Destination
-	var source1 = context.createMediaElementSource(audioElement);
-	source1.connect(analyser);
-	analyser.connect(context.destination);
-	freqAnalyser();
-	// Initial call
-	respondCanvas();
-	// Run function when browser resizes
-	$(window).resize(respondCanvas);
-	$("#headerLiveTrackHolder").airtimeLiveTrackInfo({
-		sourceDomain: "http://radio.riocast.net",
-		text: {
-			onAirNow: "AL AIRE",
-			offline: "Offline",
-			current: "Reproduciéndo ahora",
-			next: "Próximamente"
-		},
-		updatePeriod: 20 // seconds
-	});
-	$("#onAirToday").airtimeShowSchedule({
-		sourceDomain: "http://radio.riocast.net",
-		updatePeriod: 20, // seconds
-		showLimit: 20
-	});
-	if (window.audioContext || window.webkitAudioContext) {
-		// console.log("audioContext Supported");
-	} else {
-		$("#vis").hide();
-	}
-});
+$("document")
+  .ready(function () {
+    // connect AudioElement to Analyzer via source1 variable then Analyzer to Destination
+    var source1 = context.createMediaElementSource(audioElement);
+    source1.connect(analyser);
+    analyser.connect(context.destination);
+    freqAnalyser();
+    // Initial call
+    respondCanvas();
+    // Run function when browser resizes
+    $(window)
+      .resize(respondCanvas);
+    $("#headerLiveTrackHolder")
+      .airtimeLiveTrackInfo({
+        sourceDomain: "http://radio.riocast.net",
+        text: {
+          onAirNow: "AL AIRE",
+          offline: "Offline",
+          current: "Reproduciéndo ahora",
+          next: "Próximamente"
+        },
+        updatePeriod: 20 // seconds
+      });
+    $("#onAirToday")
+      .airtimeShowSchedule({
+        sourceDomain: "http://radio.riocast.net",
+        updatePeriod: 20, // seconds
+        showLimit: 20
+      });
+    if (window.audioContext || window.webkitAudioContext) {
+      // console.log("audioContext Supported");
+    } else {
+      $("#vis")
+        .hide();
+    }
+  });
